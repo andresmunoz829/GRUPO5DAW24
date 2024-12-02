6729d082c0fc16b9646d0a29e42e9252a1c05aaa
Pablo Ignacio <ignacio.quintero.pablo@iescamas.es>
2024-12-02 13:00:27 +0100

    Añadidos archivos de integrante3

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "localhost";
+     static public $mvc_bd_nombre   = "alimentos52024";
+     static public $mvc_bd_usuario  = "myagutbar94";
+     static public $mvc_bd_clave    = "gQj3cEY6";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
